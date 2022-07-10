ccf3eef7a7857e497e5a87997d578e51431b912b
leerivera <leerivera80@gmail.com>
2022-07-10 09:09:20 -0400

    solution2

--- logicdrillstrafficlight.js
+++ logicdrillstrafficlight.js
@@ -5,3 +5,14 @@ const updateLight = current => {
         green: 'yellow'
     }[current];
 }
+
+function updateLight(current) {
+    switch (current) {
+        case 'green':
+            return 'yellow';
+        case 'yellow':
+            return 'red';
+            default:
+                return 'green';
+    }
+}
