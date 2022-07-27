02122705243c1807696b6d579927342659f08299
leerivera <leerivera80@gmail.com>
2022-07-27 08:35:15 -0400

    replace2

--- replace.js
+++ replace.js
@@ -3,3 +3,14 @@
 function replace(s){
     return s.replace(/[aeoiu]/ig, '!')  
   }
+  
+  
+  function replace(s){
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    for(let i = 0; i < s.length; i++) {
+      if(vowels.includes(s[i])) {
+        s = s.replace(s[i], '!');
+      }
+    }
+    return s;
+  }
