243b81febe7069c1b7805e6a65f86e7c1197c639
leerivera <leerivera80@gmail.com>
2022-08-06 09:33:17 -0400

    nsol 2

--- enum25.js
+++ enum25.js
@@ -1,10 +1,11 @@
 
 // Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
-<<<<<<< HEAD
-//
-=======
 
->>>>>>> 1694e8bd4d9edd1d3145e4dae16ca6ac50f9c209
+
 function take(arr, n){
     return arr.slice(0,n)
 }
+
+function take(arr, n) {
+    return arr.filter((elem, i) => i < n )
+  }
