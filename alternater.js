4c267a6c728acd186cb5d32904482463d7357cfb
leerivera <leerivera80@gmail.com>
2022-08-21 03:45:12 -0400

    solution 2

--- alternater.js
+++ alternater.js
@@ -10,3 +10,17 @@ String.prototype.toAlternatingCase = function () {
     // Define your method here :)
     return this.split('').map(x => x == x.toUpperCase() ? x.toLowerCase() : x.toUpperCase()).join('')
   }
+
+
+  String.prototype.toAlternatingCase = function () {
+    new_str = "";
+    for(var i = 0; i < this.length; i++) {
+      if(this[i] === this[i].toUpperCase()) {
+        new_str += this[i].toLowerCase();
+      }
+      else {
+        new_str += this[i].toUpperCase();
+      }
+    }
+    return new_str;
+  }
