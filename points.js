a1019aa987871a91bd5b91ce1df3176c6c5f54d9
leerivera <leerivera80@gmail.com>
2022-08-22 03:45:12 -0400

    sol with map

--- points.js
+++ points.js
@@ -24,4 +24,18 @@ function points(games) {
     return games.reduce((w, [x, _, y]) => w + (x > y ? 3 : x ==y), 0)
   }
 
+//using map
+
+function points(games) {
+    let total = 0;
+    games.map(game => {
+      if (game[0] === game[2]) {
+        total += 1;
+      } else if (game[0] > game[2]) {
+        total += 3;
+      }
+    });
+    return total;
+  }
+
 
