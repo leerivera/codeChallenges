f5be3d46ba50613d08f8df8bd11a309ad4e92acb
leerivera <leerivera80@gmail.com>
2022-08-22 03:45:12 -0400

    sol 3 for loop

--- points.js
+++ points.js
@@ -38,4 +38,17 @@ function points(games) {
     return total;
   }
 
+  // with for loop
+  function points(games) {
+    var sum=0;
+    for (var i=0; i<games.length; ++i)
+    {
+      if (games[i][0]>games[i][2])
+        sum+=3;
+      if (games[i][0]==games[i][2])
+        sum+=1;
+    }
+    return sum;
+  }
+
 
