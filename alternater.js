5be4bd1a6eadb369fc0598b8cff5d348e45e2e73
leerivera <leerivera80@gmail.com>
2022-10-17 21:38:14 -0400

    alternatecase

---
+++ alternater.js
@@ -0,0 +1,12 @@
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+// "hello world".toAlternatingCase() === "HELLO WORLD"
+// "HELLO WORLD".toAlternatingCase() === "hello world"
+// "hello WORLD".toAlternatingCase() === "HELLO world"
+// "HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+
+
+String.prototype.toAlternatingCase = function () {
+    // Define your method here :)
+    return this.split('').map(x => x == x.toUpperCase() ? x.toLowerCase() : x.toUpperCase()).join('')
+  }
